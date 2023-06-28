63fb34b9d82116fa4efa48eca7d824125f571f6b
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-06-27 22:09:40 -0300

    Aula JavaScript parte1- aula 1
    Lógica de programação, O que são variáveis, String, Template Strings, Numbers, Boolean.

---
+++ 04_JavaScript/Parte_01/01_variaveis.js
@@ -0,0 +1,11 @@
+/*
+    Variáveis
+
+        - let -> Podemos declarar variáveis com escopo de bloco.
+        - const -> Não pode ser atualizado nem declarado novamente.
+        - var -> Descotinuado / Não usar.
+*/
+
+let numero = 40
+
+console.log(numero)
