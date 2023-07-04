8b15830b183285007f5b6d4dbd5d51ada3b837ef
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-07-03 22:03:49 -0300

    Aula JavaScript parte3- aula 1
    Math

---
+++ 04_JavaScript/Parte_03/01_math.js
@@ -0,0 +1,18 @@
+/*
+    Math -> Matemática
+    -pow(2,2) / potência / 2² = 4
+    -sqrt(25,2) / raiz / √25
+    -PI
+    -ceil / teto -> arredondar pra cima
+    -floor / chão -> arredondar pra baixo
+
+    -random() / número aleatório entre 0 e 1
+*/
+
+//const result = Math.pow(2,2)
+//const result = Math.sqrt(25)
+//const result = Math.PI
+//const result = Math.ceil(3.14)
+const result = Math.random()
+
+console.log(result)
