99bb9de041a93bb4a7682b18c7a3234959a7e554
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-07-17 21:41:40 -0300

    Aula Back-and: Node - aula 3
    Projeto com Node, Middlewares, Debug, Gitignore.

--- 05_back-end/node/Introducao-node/index.js
+++ 05_back-end/node/Introducao-node/index.js
@@ -15,11 +15,42 @@ app.post('/users', (req, res) => {
 
     const { name, age } = req.body
 
-    const user = { id:uuid.v4(), name, age }
+    const user = { id: uuid.v4(), name, age }
 
     users.push(user)
 
     return res.status(201).json(user)
+})
+
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, age } = req.body
+
+    const updateUser = { id, name, age }
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return res.status(404).json({ message: "User not found" })
+    }
+        
+    users[index] = updateUser
+
+    return res.json(updateUser)
+})
+
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return res.status(404).json({ message: "User not found" })
+    }
+
+    users.splice(index,1)
+
+    return res.status(204).json()
 })
 
 
