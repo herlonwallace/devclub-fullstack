91461a4186f07de9a9345e3b0858ef6c910cabac
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-07-25 22:02:23 -0300

    Aula Front-end: React- aula 3
    Imagens no React, Estilos globais no React, Estilizando a aplicação.

--- 07_front-end/02_React/first-project-react/src/App.js
+++ 07_front-end/02_React/first-project-react/src/App.js
@@ -1,10 +1,13 @@
 import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from "./style"
 
+import People from './assets/people.svg'
+import Arrow from './assets/arrow.svg'
+
 function App() {
 
   return (
     <Container>
-      <Image />
+      <Image alt="logo-imagem" src={People} />
       <ContainerItens>
         <H1>Olá!</H1>
 
@@ -14,7 +17,7 @@ function App() {
         <InputLabel>Idade</InputLabel>
         <Input placeholder="Idade" />
 
-        <Button>Cadastrar</Button>
+        <Button>Cadastrar<img alt="seta" src={Arrow} /></Button>
 
       </ContainerItens>
     </Container>
